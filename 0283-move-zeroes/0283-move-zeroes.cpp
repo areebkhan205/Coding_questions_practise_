ea8f27769819bb12aebd561373348b394bf93cbc
areeb khan <areeb.2854.com@gmail.com>
2026-07-15 14:35:01 +0530

    Time: 1 ms (17.69%), Space: 25 MB (5.06%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int cnt=0;
+        for(int x:nums){
+            if(x==0)cnt++;
+        }
+        vector<int>temp;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+              temp.push_back(nums[i]);
+            }
+        }
+        while(cnt--){
+            temp.push_back(0);
+        }
+        nums=temp;
+    }
+};
