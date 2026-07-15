91924c33964658b1cb05fc53c99ae63dd8f4d152
areeb khan <areeb.2854.com@gmail.com>
2026-07-15 14:38:27 +0530

    Time: 0 ms (100%), Space: 24 MB (19.31%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int cnt=0;
-        for(int x:nums){
-            if(x==0)cnt++;
-        }
-        vector<int>temp;
+        // int cnt=0;
+        // for(int x:nums){
+        //     if(x==0)cnt++;
+        // }
+        // vector<int>temp;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]!=0){
+        //       temp.push_back(nums[i]);
+        //     }
+        // }
+        // while(cnt--){
+        //     temp.push_back(0);
+        // }
+        // nums=temp;
+
+        int j=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=0){
-              temp.push_back(nums[i]);
+                swap(nums[i],nums[j]);
+                j++;
             }
         }
-        while(cnt--){
-            temp.push_back(0);
-        }
-        nums=temp;
     }
 };
