a3933e2a434cfaf1ba9b6f968471daca0dfecf82
areeb khan <areeb.2854.com@gmail.com>
2026-07-22 15:50:13 +0530

    Time: 0 ms (100%), Space: 24.2 MB (45.1%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -2,17 +2,26 @@ class Solution {
 public:
     bool canPlaceFlowers(vector<int>& f, int n) {
         int a = f.size();
-
+        if (n == 0)
+            return true;
         for (int i = 0; i < a; i++) {
-            if (f[i] == 0 &&
-                (i == 0 || f[i - 1] == 0) &&
-                (i == a - 1 || f[i + 1] == 0)) {
+            if (f[i] == 0) {
+                bool leftkhali = (i==0 || f[i - 1] == 0 );
+                bool rightkhali = (i==a-1 || f[i + 1] == 0);
 
-                f[i] = 1;
-                n--;
+                if (leftkhali && rightkhali) {
+                    f[i]=1;
+                    n--;
+
+
+                    if (n == 0)
+                    return true;
+                }
+
+                
             }
         }
 
-        return n <= 0;
+        return false;
     }
 };
