bb14ea5f74de68debde9f87dd560a39c17460b29
areeb khan <areeb.2854.com@gmail.com>
2026-07-26 01:15:30 +0530

    Time: 27 ms (45.49%), Space: 18.2 MB (88.72%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -7,27 +7,38 @@ public:
         int n=grid[0].size();
               vector<vector<int>> dist(m,vector<int>(n,1e9));
               dist[0][0]=0;
-             deque<pair<int,int>> dq;
-             dq.push_back({0,0});
-             int cost=0;
-             while(!dq.empty()){
-                 auto[r,c]=dq.front();
-                 dq.pop_front();
-                 for(int i=0;i<4;i++){
-                    int nr=r+dr[i];
-                    int nc=c+dc[i];
-                    if(nr>=m || nc>=n || nr<0 || nc<0)continue;
-                    int edg=(grid[r][c]==i+1)?0:1;
-                   if (dist[r][c] + edg < dist[nr][nc]) {
-                    dist[nr][nc] = dist[r][c] + edg;
+             priority_queue<
+              pair<int,pair<int,int>>,  // cost (i,j)  the cost to reach i and j  and min heap top min cist
+              vector< pair<int,pair<int,int>>>,
+              greater< pair<int,pair<int,int>>>
+             >  pq;
+             pq.push({0,{0,0}});
 
-                    if (edg == 0)
-                        dq.push_front({nr, nc});
-                    else
-                        dq.push_back({nr, nc});
+             while(!pq.empty()){
+                auto[cost,cell]=pq.top();
+                  pq.pop();
+                  int r=cell.first;
+                  int  c=cell.second;
+
+                  if(cost>dist[r][c])continue;
+              
+                  
+
+                for(int i=0;i<4;i++){
+                       int nr=r+dr[i];
+                       int nc=c+dc[i];
+
+                       if(nr<0 || nr>=m || nc<0 || nc>=n)continue;
+
+                       int  newcost=(grid[r][c]==i+1)?0:1;  //if  same cell as the direction else change it to 1
+
+                       if(dist[r][c]+newcost<dist[nr][nc]){
+                          dist[nr][nc]=dist[r][c]+newcost;
+                          pq.push({dist[nr][nc],{nr,nc}});
+                       }
                 }
-                 }
              }
-             return dist[m-1][n-1];
+    return dist[m-1][n-1];
+
     }
 };
