a8c84b5ee839c670e55c910fc9a313cfefb283ea
areeb khan <areeb.2854.com@gmail.com>
2026-07-27 22:39:32 +0530

    Time: 3 ms (0.71%), Space: 8.3 MB (10.67%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int maximumSwap(int num) {
-        vector<string> ans;
         
-        string s=to_string(num);
-        int n=s.size();
-        int idx;
+        vector<string> temp;
+        string a=to_string(num);
+        int n=a.size();
         for(int i=0;i<n;i++){
-              string a=s;
-               idx=-1;
+               string s=a;
+               int idx=-1;
             for(int j=i+1;j<n;j++){
-                if(a[j]>a[i]){
-                if(idx==-1 || a[j]>=a[idx]){
-                idx=j;
-            }
-                }
+                 if(s[j]>s[i]){
+                        if(idx==-1 || s[j]>=s[idx]){
+                               idx=j;
+                        }
+
+                 }
             }
             if(idx!=-1){
-            swap(a[idx],a[i]);
+                swap(s[idx],s[i]);
             }
-           
-            ans.push_back(a);
+            temp.push_back(s);
+
         }
 
         int maxi=INT_MIN;
-        for(int i=0;i<ans.size();i++){
-              int as=stoi(ans[i]);
-              maxi=max(maxi,as); 
+        for(int i=0;i<temp.size();i++){
+
+               maxi=max(maxi,stoi(temp[i]));
         }
     return maxi;
     }
