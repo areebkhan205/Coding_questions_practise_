6eb01beed7257c8d64469827366247ad3f6fe43c
areeb khan <areeb.2854.com@gmail.com>
2026-08-08 20:12:24 +0530

    Time: 94 ms (23.53%), Space: 76 MB (45.87%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string a) {
+           string s=a;
+           reverse(s.begin(),s.end());
+           int n=a.size();
+           int m=s.size();
+         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+  
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=m;j++){
+            if(a[i-1]==s[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+    }
+}
+   return dp[n][m];
+    }
+};
