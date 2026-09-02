5589fe6c0ea21979ee7077a498b072da0c634b41
areeb khan <areeb.2854.com@gmail.com>
2026-09-02 22:37:58 +0530

    Time: 16 ms (8.22%), Space: 24.5 MB (7.49%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+           map<int,int>mpp;
+           for(int x:nums){
+              mpp[x]++;
+           }
+
+           for(auto it: mpp){
+            if(it.second==1)return it.first;
+           }
+    return -1;
+    }
+};
