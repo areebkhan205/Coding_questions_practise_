e86dc8bbf76db2a173901e61a5b3a418ab52dfa4
areeb khan <areeb.2854.com@gmail.com>
2026-09-03 19:36:18 +0530

    Time: 0 ms (100%), Space: 16.8 MB (69.22%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,40 +5,43 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        queue<pair<int,pair<int,int>>> qu;
+          queue<pair<int,pair<int,int>>> qu;
+        //step i and j
 
-        for(int i=0;i<m;i++){
+         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
-                 qu.push({0,{i,j}});
+                qu.push({0,{i,j}});
                 }
             }
-        }
+         }
         int maxtime=0;
-        while(!qu.empty()){
-            int time=qu.front().first;
-            int row=qu.front().second.first;
-            int col=qu.front().second.second;
-            qu.pop();
-                 maxtime=max(time,maxtime);
-            for(int i=0;i<4;i++){
-                int nr=dr[i]+row;
-                int nc=dc[i]+col;
-                if(nr<m && nc<n && nr>=0 && nc>=0 && grid[nr][nc]==1){
-                    grid[nr][nc]=2;
-                    qu.push({time+1,{nr,nc}});
-                }
-            }
-        }
+           while(!qu.empty()){
+               int  step=qu.front().first;
+                int row=qu.front().second.first;
+                int col=qu.front().second.second;
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                return -1;
+                qu.pop();
+                maxtime=max(step,maxtime);
+                for(int i=0;i<4;i++){
+                    int nr=dr[i]+row;
+                    int nc=dc[i]+col;
+                    if(nr<m && nc<n && nr>=0 && nc>=0 && grid[nr][nc]==1){
+                           grid[nr][nc]=2;
+                           qu.push({step+1,{nr,nc}});
+                    }
                 }
-            }
-        }
+           }
+
+             for(int i=0;i<m;i++){
+                 for(int j=0;j<n;j++){
+                     if(grid[i][j]==1){
+                        return -1;
+                        break;
+                     }
+
+                 }
+             }
     return maxtime;
-
     }
 };
