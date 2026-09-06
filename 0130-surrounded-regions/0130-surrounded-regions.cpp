662caf8703be3ecd4bfe0dadb22d8a316e1d3371
areeb khan <areeb.2854.com@gmail.com>
2026-09-06 16:33:48 +0530

    Time: 0 ms (100%), Space: 14.5 MB (47.34%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,49 +1,52 @@
 class Solution {
-    int dr[4] = {0, 1, 0, -1};
-    int dc[4] = {-1, 0, 1, 0};
-
-
+    int dr[4]={0,1,0,-1};
+    int dc[4]={-1,0,1,0};
 public:
-    void solve(vector<vector<char>>& b) {
- 
-        int m = b.size();
-        int n =b[0].size();
-        queue<pair<int, int>> qu;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if ((i == m - 1 || j == n - 1 || i==0 || j==0) && b[i][j]=='O') {
-                    b[i][j]='A';
-                    qu.push({i, j});
+    void solve(vector<vector<char>>& board) {
+          int  m=board.size();
+          int n=board[0].size();
+            queue<pair<int,int>> qu;
+
+            for(int i=0;i<m;i++){
+                for(int j=0;j<n;j++){
+                     if((i==m-1 || j==n-1 ||  i==0 || j==0) && board[i][j]=='O'){
+                        board[i][j]='A';
+                         qu.push({i,j});
+                     }
                 }
             }
-        }
+            while(!qu.empty()){
+                 int row=qu.front().first;
+                 int col=qu.front().second;
+                  
 
-        while (!qu.empty()) {
-            auto [row, col] = qu.front();
-            qu.pop();
-            for (int i = 0; i < 4; i++) {
-                int nr = row + dr[i];
-                int nc = col + dc[i];
+                  qu.pop();
 
-                if (nr >= 0 && nc >= 0 && nr <= m - 1 && nc <= n - 1 &&
-                    b[nr][nc] == 'O') {
-                    b[nr][nc] = 'A';
-                    qu.push({nr,nc});
-                }
+                  for(int i=0;i<4;i++){
+                      int nr=dr[i]+row;
+                      int nc=dc[i]+col;
+
+
+                      if(nr>=0 && nc>=0 && nr<m && nc<n && board[nr][nc]=='O'){
+                          board[nr][nc]='A';
+                          qu.push({nr,nc});
+                      }
+                  }
             }
-        }
 
-          for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(b[i][j]=='O')b[i][j]='X';
-            }
-        }
-            
-
-                for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if(b[i][j]=='A')b[i][j]='O';
-            }
-        }
+             for(int i=0;i<m;i++){
+                 for(int j=0;j<n;j++){
+                      if(board[i][j]=='O'){
+                           board[i][j]='X';
+                      }
+                 }
+             }
+               for(int i=0;i<m;i++){
+                 for(int j=0;j<n;j++){
+                      if(board[i][j]=='A'){
+                           board[i][j]='O';
+                      }
+                 }
+             }
     }
 };
