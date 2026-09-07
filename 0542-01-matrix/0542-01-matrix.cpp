5d6e53668db2ecd617a68aae56820b5e08e33f8f
areeb khan <areeb.2854.com@gmail.com>
2026-09-07 19:16:39 +0530

    Time: 9 ms (88.73%), Space: 35.8 MB (60.32%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -3,36 +3,39 @@ class Solution {
     int dc[4]={-1,0,1,0};
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
+              int m =mat.size();
+              int n=mat[0].size();
 
-        vector<vector<int>> dist(m,vector<int>(n,0));
-        vector<vector<int>> vis(m,vector<int>(n,0));
-       queue<pair<pair<int,int>,int>> qu;
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                 if(mat[i][j]==0){
-                    vis[i][j]=1;
-                    qu.push({{i,j},0});
+              queue<pair<pair<int,int>,int>> qu;
+              vector<vector<int>> dist(m,vector<int>(n,1e9));
+              for(int i=0;i<m;i++){
+                 for(int j=0;j<n;j++){
+                        if(mat[i][j]==0){
+                          qu.push({{i,j},0});
+                          dist[i][j]=0;
+                        }
                  }
-            }
-        }
-        while(!qu.empty()){
-         auto[cell,step]=qu.front();
+              }
 
-               dist[cell.first][cell.second]=step;
-         qu.pop();
-         for(int i=0;i<4;i++){
-            int nr=dr[i]+cell.first;
-            int nc=dc[i]+cell.second;
-            if(nr>=0 && nr<m && nc>=0 && nc<n && mat[nr][nc]==1 && !vis[nr][nc]){
-                    vis[nr][nc]=1;
-                   qu.push({{nr,nc},step+1});
-            }
-         }
-        }
+              while(!qu.empty()){
+                   int row=qu.front().first.first;
+                   int col=qu.front().first.second;
+                   int step=qu.front().second;
+                   qu.pop();
+
+                   for(int i=0;i<4;i++){
+                     int nr=dr[i]+row;
+                     int nc=dc[i]+col;
+                     if(nr<m && nc<n && nr>=0 && nc>=0){
+                        if(dist[nr][nc]>step+1){
+                          dist[nr][nc]=step+1;
+                            qu.push({{nr,nc},step+1});
+                        }
+                         
+                     }
+                   }
+
+              }
     return dist;
-
     }
 };
